85486b57ef1db50d6a10bdf1792facccb0c02aae
Alexander Barannik <barannik@corp.mail.ru>
2016-11-15 17:43:34 +0300

    v0.0001

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+	bot, err := tgbotapi.NewBotAPI("ТОКЕН")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	ucfg := tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(ucfg)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
